16522e3520ecbfffed29561ee11e1a65e345fac3
ziad <mziad.ext@eventumsolutions.com>
2023-10-04 21:12:10 +0300

    organize the inner comand components

--- skrew-ui/src/Components/CardContent/ComandContent/SwapComandContent.jsx
+++ skrew-ui/src/Components/CardContent/ComandContent/SwapComandContent.jsx
@@ -1,15 +1,27 @@
 import { Grid, Typography } from '@mui/material'
 import React from 'react'
 
+function swapLogo(degree) {
+  return(
+    <img draggable={'false'} alt='' src={require('../../../Assets/swap.png')} style={{width: "7rem", transform: `rotate(${degree}deg)`}}/>
+  )
+}
+
+const swapText = (text) => {
+  return(
+      <Typography fontSize={'4.75rem'} fontFamily={'Amiri, serif'} variant='h2' fontWeight={'bold'} color={'#3f1561'}>{`${text}`}</Typography>
+    )
+}
+
 export default function SwapComandContent() {
   return (
     <Grid item display={'flex'} alignItems={'center'} justifyContent={'space-between'} flexDirection={'column'} height={'27rem'} width={'13.9rem'}>
-        <img draggable={'false'} alt='' src={require('../../../Assets/swap.png')} style={{width: "7rem", transform: "rotate(45deg)"}}/>
+        {swapLogo(45)}
         <Grid item display={'flex'} alignItems={'center'} flexDirection={'column'} >
-            <Typography fontSize={'4.75rem'} fontFamily={'Amiri, serif'} variant='h2' fontWeight={'bold'} color={'#3f1561'}>خد</Typography>
-            <Typography fontSize={'4.75rem'} fontFamily={'Amiri, serif'} variant='h2' fontWeight={'bold'} color={'#3f1561'}>وهات</Typography>
+          {swapText("خد")}
+          {swapText("وهات")}
         </Grid>
-        <img draggable={'false'} alt='' src={require('../../../Assets/swap.png')} style={{width: "7rem", transform: "rotate(225deg)"}}/>
+        {swapLogo(225)}
     </Grid>
   )
 }
--- skrew-ui/src/Components/CardContent/ComandContent/WachOneForEveryoneComandContent.jsx
+++ skrew-ui/src/Components/CardContent/ComandContent/WachOneForEveryoneComandContent.jsx
@@ -1,12 +1,19 @@
 import { Grid, Typography } from '@mui/material'
 import React from 'react'
 
+
+const comandText = (text) => {
+  return (
+    <Typography fontSize={'5.75rem'} fontFamily={'Amiri, serif'} fontWeight={'bold'} color={'#3f1561'}>{`${text}`}</Typography>
+  )
+}
+
 export default function WachOneForEveryoneComandContent() {
   return (
     <Grid item>
       <Grid item display={'flex'} direction={'column'} alignItems={'center'} width={'13.9rem'}>
-        <Typography fontSize={'5.75rem'} fontFamily={'Amiri, serif'} fontWeight={'bold'} color={'#3f1561'}>كعب</Typography>
-        <Typography fontSize={'5.75rem'} fontFamily={'Amiri, serif'} fontWeight={'bold'} color={'#3f1561'}>داير</Typography>
+        {comandText("كعب")}
+        {comandText("داير")}
       </Grid>
     </Grid>
   )
