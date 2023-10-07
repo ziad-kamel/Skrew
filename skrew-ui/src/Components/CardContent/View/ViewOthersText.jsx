7d18d22e639c320d4c1fd08ca11e36cf15d705f8
ziad <mziad.ext@eventumsolutions.com>
2023-10-08 00:52:06 +0300

    fix postions

--- skrew-ui/src/Components/CardContent/View/ViewOthersText.jsx
+++ skrew-ui/src/Components/CardContent/View/ViewOthersText.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function ViewOthersText({cardText, cardTextColor}) {
   return (
-    <Grid item className='path' color={cardTextColor} position={"absolute"} top={"70px"} >
+    <Grid item className='path' color={cardTextColor} position={"absolute"} top={"73px"} >
         <Typography fontSize={"1.9rem"} fontFamily={"Amiri"} fontWeight={'bold'}>{cardText}</Typography>
   </Grid>
   )
